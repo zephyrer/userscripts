c688944937da7cead2e64e29897fecb997e1fe30
zephyrer <zephyrer@gmail.com>
2013-11-27 08:06:26 +0800

    Create Auto_scoreing_on_Qidian.user.js

---
+++ Auto_scoreing_on_Qidian.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name        Auto-scoreing on Qidian
+// @namespace   zephyrer@msn.com
+// @description 自动获取起点经验值
+// @include     http://me.qidian.com/profile/score.aspx
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+(function() {
+  function autoscore() {
+    var plusItems = document.getElementsByClassName('plus-items')[0];
+    var btns = plusItems.getElementsByClassName('btn');
+    for (var i=0; i<btns.length; i++) {
+      if (btns[i].textContent === '可领取') {
+        btns[i].click();
+        setTimeout("location.reload(true);", 1000);
+        break;
+      } else if (btns[i].textContent === '00:00') {
+        location.reload(true);
+        break;
+      }
+    }
+  }
+  
+  setTimeout(autoscore, 5000);  
+})();
