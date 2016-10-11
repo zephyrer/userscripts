3d95b2f17a346e771232f87cd4de2047634e8343
Efisio <zephyrer@gmail.com>
2016-10-11 23:42:05 +0800

    add CleanHiddenTexts
    CleanHiddenTexts could remove hidden contents from pages.  And it has a
    simple right-click & copy enable function.

---
+++ CleanHiddenTexts.meta.js
@@ -0,0 +1,14 @@
+﻿// ==UserScript==
+// @name          CleanHiddenTexts & Enable RightClick-Copy
+// @author        zephyrer
+// @namespace     https://github.com/zephyrer
+// @description   clean hidden texts. Hotkey:CRTL+Q. Created By Inorth. blueicejin@163.com.http://notebk.spaces.live.com/
+// @include       *
+// @version       0.5
+// @note          20161008, add auto-execution for specific sites (see sites object).
+// @grant         GM_registerMenuCommand
+// @downloadURL   https://github.com/zephyrer/userscripts/raw/master/CleanHiddenTexts.user.js
+// @updateURL     https://github.com/zephyrer/userscripts/raw/master/CleanHiddenTexts.meta.js
+// @run-at        document-end
+// @copyright     Efisio Zephyr
+// ==/UserScript==
