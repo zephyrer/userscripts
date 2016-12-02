22255d9b6775dd47ef1bfdb33e7f94346afe86ca
Efisio <zephyrer@gmail.com>
2016-12-02 13:01:33 +0800

    Update qidian.com read page urls
    qidian.com read page urls changed from BookReader to chapter

--- anti-decontext.user.js
+++ anti-decontext.user.js
@@ -4,11 +4,13 @@
 // @description    Restore context menus on sites that try to disable them
 // @include        http://vipreader.qidian.com/BookReader/*
 // @include        http://read.qidian.com/BookReader/*
+// @include        http://vipreader.qidian.com/chapter/*
+// @include        http://read.qidian.com/chapter/*
 // @downloadURL    https://github.com/zephyrer/userscripts/raw/master/anti-decontext.user.js
 // @updateURL      https://github.com/zephyrer/userscripts/raw/master/anti-decontext.meta.js
 // @require        https://raw.githubusercontent.com/joesimmons/jsl/master/versions/jsl-1.3.1.js
 // @copyright      Efisio Zephyr
-// @version        0.1.2
+// @version        0.1.3
 // @grant          none
 // @run-at         document-start
 // ==/UserScript==
