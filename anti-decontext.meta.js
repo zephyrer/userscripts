bc77a94b6d2fcf50fe6e48e0abad3d99d7a435b7
Efisio <zephyrer@gmail.com>
2017-04-17 14:06:00 +0800

    Add support for zongheng.com
    Add support for zongheng.com bookreader

--- anti-decontext.meta.js
+++ anti-decontext.meta.js
@@ -6,11 +6,12 @@
 // @include        http://read.qidian.com/BookReader/*
 // @include        http://vipreader.qidian.com/chapter/*
 // @include        http://read.qidian.com/chapter/*
+// @include        http://book.zongheng.com/chapter/*
 // @downloadURL    https://github.com/zephyrer/userscripts/raw/master/anti-decontext.user.js
 // @updateURL      https://github.com/zephyrer/userscripts/raw/master/anti-decontext.meta.js
 // @require        https://raw.githubusercontent.com/joesimmons/jsl/master/versions/jsl-1.3.1.js
 // @copyright      Efisio Zephyr
-// @version        0.1.3
+// @version        0.1.4
 // @grant          none
 // @run-at         document-start
 // ==/UserScript==
