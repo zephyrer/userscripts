2a012f6670951367d3ce4db44b756efe92f3b43c
Efisio <zephyrer@gmail.com>
2017-04-18 22:06:02 +0800

    Add AdBlocker-SelfAnti
    This is used to inject some pseudo-ad-elements to deceive the page
    ad-blocker detection mechanism.

---
+++ AdBlocker-SelfAnti.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        Anti-AdBlocker Selfly
+// @namespace   zephyrer@msn.com
+// @dscription  阻止网站反广告检测
+// @include     http://www.hdmee.com/*
+// @include     http://bbs.zzznan.com/*
+// @include     http://*.tt1069.com/*
+// @downloadURL https://github.com/zephyrer/userscripts/raw/master/AdBlocker-SelfAnti.user.js
+// @updateURL   https://github.com/zephyrer/userscripts/raw/master/AdBlocker-SelfAnti.meta.js
+/// @version     0.1.0
+// @grant       none
+// @run-at      document-start
+// ==/UserScript==
---
+++ AdBlocker-SelfAnti.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name        Anti-AdBlocker Selfly
+// @namespace   zephyrer@msn.com
+// @dscription  阻止网站反广告检测
+// @include     http://www.hdmee.com/*
+// @include     http://bbs.zzznan.com/*
+// @include     http://*.tt1069.com/*
+// @downloadURL https://github.com/zephyrer/userscripts/raw/master/AdBlocker-SelfAnti.user.js
+// @updateURL   https://github.com/zephyrer/userscripts/raw/master/AdBlocker-SelfAnti.meta.js
+// @version     0.1.0
+// @grant       none
+// @run-at      document-start
+// ==/UserScript==
+
+if (location.host.indexOf("hdmee.com") !== -1 ||
+    location.host.indexOf("zzznan.com") !== -1
+   ) {
+	window.ad = "Anti-unadblock - ad";
+	window.ads ='Anti-unadblock - ads';
+	window.dzad ='Anti-unadblock - dzad';
+	window.dzads ='Anti-unadblock - dzads';
+}
+else if (location.host.indexOf("tt1069.com") !== -1) {
+	let adLayer = document.createElement("DIV");
+	adLayer.id = "tt_FIuBYoHOADK";
+	document.body.appendChild(adLayer);
+}
+//alert(window.ad);
