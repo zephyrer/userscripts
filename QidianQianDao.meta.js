a1d8c5b981c8f8ffdb6df8714c65bde44ffe197c
Efisio <zephyrer@gmail.com>
2017-05-26 13:15:09 +0800

    Update for supporting new site style
    Qidian updates their site look and architectures, so this script must be
    updated to apply new styles.

--- QidianQianDao.meta.js
+++ QidianQianDao.meta.js
@@ -3,7 +3,8 @@
 // @namespace   https://github.com/zephyrer
 // @description 自动获取起点经验值
 // @include     http://me.qidian.com/profile/score.aspx
-// @version     0.2.0
+// @include     http://my.qidian.com/level
+// @version     0.3.0
 // @copyright   Efisio Zephyr
 // @downloadURL https://github.com/zephyrer/userscripts/raw/master/QidianQianDao.user.js
 // @updateURL   https://github.com/zephyrer/userscripts/raw/master/QidianQianDao.meta.js
--- QidianQianDao.user.js
+++ QidianQianDao.user.js
@@ -3,7 +3,8 @@
 // @namespace   https://github.com/zephyrer
 // @description 自动获取起点经验值
 // @include     http://me.qidian.com/profile/score.aspx
-// @version     0.2.0
+// @include     http://my.qidian.com/level
+// @version     0.3.0
 // @copyright   Efisio Zephyr
 // @downloadURL https://github.com/zephyrer/userscripts/raw/master/QidianQianDao.user.js
 // @updateURL   https://github.com/zephyrer/userscripts/raw/master/QidianQianDao.meta.js
@@ -12,19 +13,28 @@
 
 (function() {
   function autoscore() {
-    var plusItems = document.getElementsByClassName('plus-items')[0];
-    var btns = plusItems.getElementsByClassName('btn');
-    for (var i=0; i<btns.length; i++) {
-      if (btns[i].textContent.includes('可领取')) {
-        btns[i].click();
+    if (location.href.indexOf("score") > -1) {
+      var plusItems = document.getElementsByClassName('plus-items')[0];
+      var btns = plusItems.getElementsByClassName('btn');
+      for (var i=0; i<btns.length; i++) {
+        if (btns[i].textContent.includes('可领取')) {
+          btns[i].click();
+          setTimeout("location.reload(true);", 1000);
+          break;
+        } else if (btns[i].textContent === '00:00') {
+          location.reload(true);
+          break;
+        }
+      }
+    } else {
+      var btns = document.getElementsByClassName('elGetExp');
+      if (btns) {
+        btns[0].click();
         setTimeout("location.reload(true);", 1000);
-        break;
-      } else if (btns[i].textContent === '00:00') {
-        location.reload(true);
-        break;
+      } else {
+        setTimeout(autoscore, 5000);
       }
     }
   }
-  
-  setTimeout(autoscore, 5000);  
+  setTimeout(autoscore, 5000);
 })();
