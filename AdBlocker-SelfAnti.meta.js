02f003c7efb9b1fb0de97028af207fd6094f28a6
Efisio <zephyrer@gmail.com>
2017-06-12 00:53:54 +0800

    Fixes a misspelling in header
    Fixes a misspelling in header

--- AdBlocker-SelfAnti.meta.js
+++ AdBlocker-SelfAnti.meta.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name        Anti-AdBlocker Selfly
 // @namespace   zephyrer@msn.com
-// @dscription  阻止网站反广告检测
+// @description  阻止网站反广告检测
 // @include     http://www.hdmee.com/*
 // @include     http://bbs.zzznan.com/*
 // @include     http://*.tt1069.com/*
 // @include     http://www.qingguox.com/*
 // @downloadURL https://github.com/zephyrer/userscripts/raw/master/AdBlocker-SelfAnti.user.js
 // @updateURL   https://github.com/zephyrer/userscripts/raw/master/AdBlocker-SelfAnti.meta.js
-// @version     0.1.1
+// @version     0.1.2
 // @grant       none
 // @run-at      document-start
 // ==/UserScript==
