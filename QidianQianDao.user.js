b1f2be928a8d7d00963283e2ee017332d25a65ed
Efisio <zephyrer@gmail.com>
2017-07-23 00:16:16 +0800

    Update QidianQianDao.user.js
    缩小轮询间距

--- QidianQianDao.user.js
+++ QidianQianDao.user.js
@@ -3,7 +3,7 @@
 // @namespace   EfisioZephyr
 // @description 自动获取起点经验值
 // @include     http://my.qidian.com/level
-// @version     0.3.2.1
+// @version     0.3.2.2
 // @copyright   Efisio Zephyr
 // @downloadURL https://github.com/zephyrer/userscripts/raw/master/QidianQianDao.user.js
 // @updateURL   https://github.com/zephyrer/userscripts/raw/master/QidianQianDao.meta.js
@@ -18,7 +18,7 @@
       btns[0].click();
       //setTimeout("location.reload(true);", 1000);
     } else if (document.getElementsByClassName('elIsCurrent')) {
-      setTimeout(autoscore, 300000);
+      setTimeout(autoscore, 30000);
     }
   }
   setTimeout(autoscore, 5000);
