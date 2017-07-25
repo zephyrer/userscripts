93a8ce104f8fd45a9d7304ccea60bf868508b64e
Efisio <zephyrer@gmail.com>
2017-07-25 09:39:12 +0800

    Fix conflicts
    Fix conflicts

--- QidianQianDao.user.js
+++ QidianQianDao.user.js
@@ -3,11 +3,7 @@
 // @namespace   EfisioZephyr
 // @description 自动获取起点经验值
 // @include     http://my.qidian.com/level
-<<<<<<< HEAD
 // @version     0.3.2.3
-=======
-// @version     0.3.2.2
->>>>>>> 1b9b29f8ca2ef43f96306998c453e6d1f569c34e
 // @copyright   Efisio Zephyr
 // @downloadURL https://github.com/zephyrer/userscripts/raw/master/QidianQianDao.user.js
 // @updateURL   https://github.com/zephyrer/userscripts/raw/master/QidianQianDao.meta.js
@@ -22,11 +18,7 @@
       btns[0].click();
       //setTimeout("location.reload(true);", 1000);
     } else if (document.getElementsByClassName('elIsCurrent')) {
-<<<<<<< HEAD
       setTimeout(autoscore, 10000);
-=======
-      setTimeout(autoscore, 30000);
->>>>>>> 1b9b29f8ca2ef43f96306998c453e6d1f569c34e
     }
   }
   setTimeout(autoscore, 1);
