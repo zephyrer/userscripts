c6fcd2a3809aee9718a29d0e7966eeba3a812579
Efisio <zephyrer@gmail.com>
2017-09-27 00:56:46 +0800

    Update QidianQianDao.user.js
    fix a bug: when first calling GM_log, btns maybe invalid, so leads script to be broken.

--- QidianQianDao.user.js
+++ QidianQianDao.user.js
@@ -4,7 +4,7 @@
 // @description 自动获取起点经验值
 // @include     http://my.qidian.com/level
 // @include     https://my.qidian.com/level
-// @version     0.3.2.4
+// @version     0.3.2.5
 // @copyright   Efisio Zephyr
 // @downloadURL https://github.com/zephyrer/userscripts/raw/master/QidianQianDao.user.js
 // @updateURL   https://github.com/zephyrer/userscripts/raw/master/QidianQianDao.meta.js
@@ -14,8 +14,8 @@
 (function() {
   function autoscore() {
     var btns = document.getElementsByClassName('elGetExp');
-    GM_log("elGetExp: " + btns.length);
     if (btns && btns.length > 0) {
+      GM_log("elGetExp: " + btns.length);
       btns[0].click();
       //setTimeout("location.reload(true);", 1000);
     } else if (document.getElementsByClassName('elIsCurrent')) {
