a69764d7b4a16e516c3e4f93cbd4f0711961f230
Efisio <zephyrer@gmail.com>
2017-09-27 00:57:48 +0800

    Update QidianQianDao.meta.js
    fix a bug: when first calling GM_log, btns maybe invalid, so leads script broken

--- QidianQianDao.meta.js
+++ QidianQianDao.meta.js
@@ -4,7 +4,7 @@
 // @description 自动获取起点经验值
 // @include     http://my.qidian.com/level
 // @include     https://my.qidian.com/level
-// @version     0.3.2.4
+// @version     0.3.2.5
 // @copyright   Efisio Zephyr
 // @downloadURL https://github.com/zephyrer/userscripts/raw/master/QidianQianDao.user.js
 // @updateURL   https://github.com/zephyrer/userscripts/raw/master/QidianQianDao.meta.js
