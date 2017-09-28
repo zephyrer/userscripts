17635c3127d479904f93d40e10be065b464839a5
Efisio <zephyrer@gmail.com>
2017-09-28 16:06:43 +0800

    Update anti-decontext.user.js
    Includes https urls of Qidian

--- anti-decontext.user.js
+++ anti-decontext.user.js
@@ -5,8 +5,11 @@
 // @include        http://vipreader.qidian.com/BookReader/*
 // @include        http://read.qidian.com/BookReader/*
 // @include        http://vipreader.qidian.com/chapter/*
+// @include        https://vipreader.qidian.com/chapter/*
 // @include        http://read.qidian.com/chapter/*
+// @include        https://read.qidian.com/chapter/*
 // @include        http://book.zongheng.com/chapter/*
+// @include        https://book.zongheng.com/chapter/*
 // @downloadURL    https://github.com/zephyrer/userscripts/raw/master/anti-decontext.user.js
 // @updateURL      https://github.com/zephyrer/userscripts/raw/master/anti-decontext.meta.js
 // @require        https://raw.githubusercontent.com/joesimmons/jsl/master/versions/jsl-1.3.1.js
