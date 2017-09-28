0c34d2a5c540f3706c853eb62b6271053f467706
Efisio <zephyrer@gmail.com>
2017-09-28 16:07:56 +0800

    Update anti-decontext.meta.js
    Includes https urls of Qidian and updates version

--- anti-decontext.meta.js
+++ anti-decontext.meta.js
@@ -1,18 +1,20 @@
-﻿// ==UserScript==
+// ==UserScript==
 // @name           Anti-DeContext
 // @namespace      EfisioZephyr
 // @description    Restore context menus on sites that try to disable them
 // @include        http://vipreader.qidian.com/BookReader/*
 // @include        http://read.qidian.com/BookReader/*
 // @include        http://vipreader.qidian.com/chapter/*
+// @include        https://vipreader.qidian.com/chapter/*
 // @include        http://read.qidian.com/chapter/*
+// @include        https://read.qidian.com/chapter/*
 // @include        http://book.zongheng.com/chapter/*
+// @include        https://book.zongheng.com/chapter/*
 // @downloadURL    https://github.com/zephyrer/userscripts/raw/master/anti-decontext.user.js
 // @updateURL      https://github.com/zephyrer/userscripts/raw/master/anti-decontext.meta.js
 // @require        https://raw.githubusercontent.com/joesimmons/jsl/master/versions/jsl-1.3.1.js
 // @copyright      Efisio Zephyr
-// @version        0.1.4.1
+// @version        0.1.4.3
 // @grant          none
 // @run-at         document-start
 // ==/UserScript==
-
