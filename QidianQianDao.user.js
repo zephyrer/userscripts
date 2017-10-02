a453d52449158c048ec1527590ab60bc0416c55a
Efisio <zephyrer@gmail.com>
2017-10-03 02:03:58 +0800

    修复bug，提高稳定性
    在运行较慢的环境下第一个“领取”有可能在jQuery未完全加载完成时就被执行了click操作，导致失败。新版不再使用setTimeout进行首次操作，改用setInterval不断检查$的有效性以保证jQuery完全载入后再执行自动签到。同时，不再采用DOM自带的click函数，而是改用jQuery的click函数来触发click事件，以求稳定——网页就是用$.on()来设定click事件处理器。

--- QidianQianDao.user.js
+++ QidianQianDao.user.js
@@ -4,26 +4,31 @@
 // @description 自动获取起点经验值
 // @include     http://my.qidian.com/level
 // @include     https://my.qidian.com/level
-// @version     0.3.2.5
+// @version     0.4.0
 // @copyright   Efisio Zephyr
 // @downloadURL https://github.com/zephyrer/userscripts/raw/master/QidianQianDao.user.js
 // @updateURL   https://github.com/zephyrer/userscripts/raw/master/QidianQianDao.meta.js
 // @grant       GM_log
+// @grant       unsafeWindow
 // ==/UserScript==
 
 (function() {
   function autoscore() {
     var btns = document.getElementsByClassName('elGetExp');
-    if (btns && btns.length > 0) {
-      GM_log("elGetExp: " + btns.length);
-      btns[0].click();
-      //setTimeout("location.reload(true);", 1000);
+    if (btns.length > 0) {
+      //GM_log("elGetExp: " + btns.length);
+      unsafeWindow.$(btns[0]).click();
+      //setTimeout("location.reload(true);", 1);
     } else if (document.getElementsByClassName('elIsCurrent')) {
       setTimeout(autoscore, 10000);
     } else {
       setTimeout(autoscore, 3600000);
     }
   }
-  setTimeout(autoscore, 10);
+  var n = setInterval(function() {
+    GM_log(unsafeWindow.$);
+    if(unsafeWindow.$ !== undefined)
+      clearInterval(n);
+      autoscore();
+  }, 100);
 })();
-
