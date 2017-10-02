86342289b734b8c9ed9e386e106f1ef321a4bb7f
Efisio <zephyrer@gmail.com>
2017-10-03 02:05:01 +0800

    修复bug，增强稳定性
    在运行较慢的环境下第一个“领取”有可能在jQuery未完全加载完成时就被执行了click操作，导致失败。新版不再使用setTimeout进行首次操作，改用setInterval不断检查$的有效性以保证jQuery完全载入后再执行自动签到。同时，不再采用DOM自带的click函数，而是改用jQuery的click函数来触发click事件，以求稳定——网页就是用$.on()来设定click事件处理器。

--- QidianQianDao.meta.js
+++ QidianQianDao.meta.js
@@ -4,9 +4,10 @@
 // @description 自动获取起点经验值
 // @include     http://my.qidian.com/level
 // @include     https://my.qidian.com/level
-// @version     0.3.2.5
+// @version     0.4.0
 // @copyright   Efisio Zephyr
 // @downloadURL https://github.com/zephyrer/userscripts/raw/master/QidianQianDao.user.js
 // @updateURL   https://github.com/zephyrer/userscripts/raw/master/QidianQianDao.meta.js
 // @grant       GM_log
+// @grant       unsafeWindow
 // ==/UserScript==
