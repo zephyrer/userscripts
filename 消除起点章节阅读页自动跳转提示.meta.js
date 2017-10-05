4f2bf54d32a52334583142c1b446a01652fc4f08
Efisio <zephyrer@gmail.com>
2017-10-05 18:09:46 +0800

    Ungrade 消除起点章节阅读页自动跳转提示 to 0.3
    修复无限循环的bug，提高慢速网络的适应性

--- 消除起点章节阅读页自动跳转提示.meta.js
+++ 消除起点章节阅读页自动跳转提示.meta.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         消除起点章节阅读页自动跳转提示
 // @namespace    EfisioZephyr
-// @version      0.2
+// @version      0.3
 // @description  消除烦人的起点章节阅读页面自动跳转对话框
 // @author       Efisio Zephyr
 // @include      https://vipreader.qidian.com/chapter/*
--- 消除起点章节阅读页自动跳转提示.user.js
+++ 消除起点章节阅读页自动跳转提示.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         消除起点章节阅读页自动跳转提示
 // @namespace    EfisioZephyr
-// @version      0.2
+// @version      0.3
 // @description  消除烦人的起点章节阅读页面自动跳转对话框
 // @author       Efisio Zephyr
 // @include      https://vipreader.qidian.com/chapter/*
@@ -13,16 +13,18 @@
 
 (function() {
   var cnt = 0;
+  var maxTime = 25;
   function doThing() {
     var b = document.getElementsByClassName("read-status-close");
+    GM_log("消除自动跳转干扰.user.js: executed " + cnt + " times...");
     if (b && (b.length > 0) && b[0]) {
-        GM_log("消除自动跳转干扰: b.length - " + b.length);
-        GM_log("消除自动跳转干扰: b[0].click - " + b[0].click.toString());
+        //GM_log("消除自动跳转干扰.user.js: b[0].click - " + b[0].click.toString());
         b[0].click();
+        cnt = maxTime;
     } else {
-        GM_log("消除自动跳转干扰: 无干扰");
+        GM_log("消除自动跳转干扰.user.js: 无干扰");
     }
-    if (cnt < 10) {
+    if (cnt < maxTime) {
        cnt++;
        setTimeout(doThing, 500);
     }
