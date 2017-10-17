0f4b289f0764103170740c86e8bb95ad2db0491c
Efisio <zephyrer@gmail.com>
2017-10-17 15:34:26 +0800

    include read.qidian.com
    Add support to non-VIP chapters

--- 消除起点章节阅读页自动跳转提示.user.js
+++ 消除起点章节阅读页自动跳转提示.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         消除起点章节阅读页自动跳转提示
 // @namespace    EfisioZephyr
-// @version      0.3
+// @version      0.4
 // @description  消除烦人的起点章节阅读页面自动跳转对话框
 // @author       Efisio Zephyr
 // @include      https://vipreader.qidian.com/chapter/*
+// @include      https://read.qidian.com/chapter/*
 // @downloadURL  https://github.com/zephyrer/userscripts/raw/master/%E6%B6%88%E9%99%A4%E8%B5%B7%E7%82%B9%E7%AB%A0%E8%8A%82%E9%98%85%E8%AF%BB%E9%A1%B5%E8%87%AA%E5%8A%A8%E8%B7%B3%E8%BD%AC%E6%8F%90%E7%A4%BA.user.js
 // @updateURL    https://github.com/zephyrer/userscripts/raw/master/%E6%B6%88%E9%99%A4%E8%B5%B7%E7%82%B9%E7%AB%A0%E8%8A%82%E9%98%85%E8%AF%BB%E9%A1%B5%E8%87%AA%E5%8A%A8%E8%B7%B3%E8%BD%AC%E6%8F%90%E7%A4%BA.meta.js
 // @grant        GM_log
