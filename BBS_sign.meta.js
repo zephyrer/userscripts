0ede8355974a2511910d1589318ac3804432e13b
Efisio <zephyrer@gmail.com>
2018-02-21 00:50:07 +0800

    Update site including rule
    Update site including rule

--- BBS_sign.meta.js
+++ BBS_sign.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         论坛签到工具
 // @namespace    https://github.com/zephyrer/
-// @version      1.6.8.8
+// @version      1.6.8.9
 // @description  用于各种论坛自动签到，自用！！
 // @include      http*://*/plugin.php?id=*sign*
 // @include      http*://*/dsu_paulsign-sign*
@@ -13,6 +13,7 @@
 // @include      http*://*/forum.php
 // @include      http*://*/u.php*
 // @include      http*://*/*qiandao
+// @include      http*://*/*qiandao&
 // @include      http*://bbs.kafan.cn/*
 // @include      http*://bbs.wstx.com/*
 // @include      http*://bbs.gfan.com/*
