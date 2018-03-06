4cb782a8ba3b8095eb41b39d1c1f20dcb7168a1b
Efisio <zephyrer@gmail.com>
2018-03-06 13:55:20 +0800

    Update @match rules
    Update @match rules

--- tumblr-dl.meta.js
+++ tumblr-dl.meta.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Tumblr Download
-// @version      0.3
+// @version      0.3.1
 // @description  Download tumblr photos by typing 'dl' when viewing them
 // @author       zephyrer
 // @namespace    https://github.com/zephyrer/
-// @match        https://www.tumblr.com/*
+// @match        https://*.tumblr.com/*
+// @match        http://*.tumblr.com/*
 // @downloadURL  https://github.com/zephyrer/userscripts/raw/master/tumblr-dl.user.js
 // @updateURL    https://github.com/zephyrer/userscripts/raw/master/tumblr-dl.meta.js
 // @copyright    2018, Efisio Zephyr
