6deb1485d92cb41eb04519ef98ed8a806810e74b
Efisio <zephyrer@gmail.com>
2018-04-26 10:38:13 +0800

    增加一个超话
    增加一个超话

--- WeiboHuatiDirectLinks.meta.js
+++ WeiboHuatiDirectLinks.meta.js
@@ -3,7 +3,7 @@
 // @author        zephyrer
 // @namespace     https://github.com/zephyrer/
 // @match         https://weibo.com/*
-// @version       0.1.3
+// @version       0.1.4
 // @description   为微博页面添加超话直链
 // @downloadURL   https://github.com/zephyrer/userscripts/raw/master/WeiboHuatiDirectLinks.user.js
 // @updateURL     https://github.com/zephyrer/userscripts/raw/master/WeiboHuatiDirectLinks.meta.js
--- WeiboHuatiDirectLinks.user.js
+++ WeiboHuatiDirectLinks.user.js
@@ -3,7 +3,7 @@
 // @author        zephyrer
 // @namespace     https://github.com/zephyrer/
 // @match         https://weibo.com/*
-// @version       0.1.3
+// @version       0.1.4
 // @description   为微博页面添加超话直链
 // @downloadURL   https://github.com/zephyrer/userscripts/raw/master/WeiboHuatiDirectLinks.user.js
 // @updateURL     https://github.com/zephyrer/userscripts/raw/master/WeiboHuatiDirectLinks.meta.js
@@ -32,13 +32,15 @@
   var huatis = [
     {name: '杨孟霖',
      url:  'http://huati.weibo.com/5450398'},
+    {name: '杨孟霖Nick',
+     url:  'http://huati.weibo.com/5479348'},
     {name: '施柏宇',
      url:  'http://huati.weibo.com/5489289'},
     {name: '宇霖cp',
      url:  'http://huati.weibo.com/5509572'},
     {name: '张赫',
      url:  'http://huati.weibo.com/5132232'},
-    {}
+    {}// ending placeholder
   ];
   
   var div = document.createElement('div');
@@ -46,7 +48,7 @@
   div.style = 'position: fixed; left: auto; right: 2px; top: auto; bottom: 100px; border: 2px red solid;';
   var list = document.createElement('ul');
   for (let i=0; i<huatis.length; i++) {
-    if (isEmpty(huatis[i])) continue;
+    if (isEmpty(huatis[i])) break;// ending placeholder
     let link = document.createElement('a');
     link.id = 'direct-link-to-super-topic-' + i;
     link.innerHTML = huatis[i].name;
