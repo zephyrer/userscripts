91ae6eaf7bc8390adb156dbd4ec8ac4435972bd4
Efisio <zephyrer@gmail.com>
2018-05-31 19:54:56 +0800

    add a logic to vidown
    add a logic to vidown to avoid redundant clicks.

--- BBS_sign.user.js
+++ BBS_sign.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         论坛签到工具
 // @namespace    https://github.com/zephyrer/
-// @version      1.6.8.67
+// @version      1.6.8.68
 // @description  用于各种论坛自动签到，自用！！
 // @include      http*://*/plugin.php?id=*sign*
 // @include      http*://*/dsu_paulsign-sign*
@@ -87,18 +87,22 @@
     }
     let count = 0;
     let iid = setInterval(function() {
-        let els = _class("right");
-        if (els) {
-          clearInterval(iid);
-          els[0].click();
-          return;
-        } else {
-          count++;
-        }
-        if (count > 50) {
-          clearInterval(iid);
-        }
-      }, 500);
+      let els = _class("right");
+      if (els) {
+        clearInterval(iid);
+	      if (els[0].textContent.includes('已签到')) {
+	        return;
+	      } else {
+	        els[0].click();
+	        return;
+	      }
+      } else {
+        count++;
+      }
+      if (count > 50) {
+        clearInterval(iid);
+      }
+    }, 500);
   }
 
   if (isURL("zsj_moneychange")) {
