23368d2abf6cac90deff469a028b4c46d799b5f7
Efisio Zephyr <zephyrer@gmail.com>
2019-01-05 09:23:23 +0800

    Update to apply huawei club changes

--- BBS_sign.user.js
+++ BBS_sign.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         论坛签到工具
 // @namespace    https://github.com/zephyrer/
-// @version      1.6.10.5
+// @version      1.6.10.6
 // @description  用于各种论坛自动签到，自用！！
 // @include      http*://*/plugin.php?id=*sign*
 // @include      http*://*/dsu_paulsign-sign*
@@ -612,6 +612,7 @@ xqqiandao: {
 
   if (isURL("club.vmall.com/plugin.php?id=dsu_paulsign:sign")
      || isURL("club.vmall.com/dsu_paulsign-sign")
+     || isURL("club.huawei.com/dsu_paulsign-sign")
      || isURL("club.huawei.com/plugin.php?id=dsu_paulsign:sign")) {
       //华为
     document.getElementsByClassName('sign-btn btn_rs')[0].click();
