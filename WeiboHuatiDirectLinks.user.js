0e15f254b445e2fc9634b966228695b01074d641
zephyrer <zephyrer@gmail.com>
2022-12-25 13:42:36 +0800

    add a weiboer about BL dramas
    add a weiboer about BL dramas to WeiboHuatiDirectLinks

--- WeiboHuatiDirectLinks.user.js
+++ WeiboHuatiDirectLinks.user.js
@@ -5,7 +5,7 @@
 // @match         https://weibo.com/*
 // @match         https://*.weibo.com/*
 // @exclude-match https://weibo.com/ttarticle/*
-// @version       0.3.1.06
+// @version       0.3.1.07
 // @description   为微博等站点页面添加各类直链
 // @icon          https://weibo.com/favicon.ico
 // @downloadURL   https://github.com/zephyrer/userscripts/raw/master/WeiboHuatiDirectLinks.user.js
@@ -85,6 +85,8 @@
        url:  'https://weibo.com/u/6451931503'},
       {name: '在太阳阳下开花',
        url:  'https://weibo.com/u/6198528359'},
+      {name: '同影三千',
+       url:  'https://weibo.com/u/5784069948'},
       {name: '霸王龙压制组',
        url:  'https://weibo.com/u/7498642767'},
       {name: '小寒追剧',
